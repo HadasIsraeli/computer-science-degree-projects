37acb81bcf5f31eb1a7e9f40a830dedc925a4989
Hadas Israeli <76647532+HadasIsraeli@users.noreply.github.com>
2021-05-13 17:21:54 +0300

    Create README.d

---
+++ Introduction to Computer Science/README.d
@@ -0,0 +1,3 @@
+This are the first projects given in Computer Science degree at the first semester as part from 
+Introduction to Computer Science course using C++/C.
+It is a good practice to know the basics of a computer coding languege. 
